913f404c3ae01cceb771f9a7a533286f06e521b4
saikitjk <saikitjk@TACOMA.UW.EDU>
2018-01-31 20:40:05 -0800

    First Commit

---
+++ Contoso1/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contoso1.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string LastName { get; set; }//Auto-implemented property
+
+        public string FirstMidName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment>Enrollments { get; set; }
+    }
+}
